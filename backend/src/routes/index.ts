ae7a9a779ac69075a1b59386c51b4e5afa9e0e19
Thiago Simoes <tps.snf23uea.edu.br>
2025-01-17 20:40:27 -0300

    create: getAllDefectiveProducts, getAllProducts, getAllWorkstations, createTray.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { getAllUsers } from "./get-all-users";
+import { getAllWorkstations } from "./get-all-workstations";
+import { getAllDefectiveProducts } from "./get-all-defectiveProducts";
+import { getAllProducts } from "./get-all-products";
 
 const routes = express.Router();
 
 routes.get("/users", getAllUsers);
+routes.get("/workStations", getAllWorkstations);
+routes.get("/defectiveProducts", getAllDefectiveProducts);
+routes.get("/products", getAllProducts);
 
 export { routes };
