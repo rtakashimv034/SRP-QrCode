bb6241dcdab2f2862d0b55bd03279f2ff6f8279c
PedroYutaroUEA <pymmn.snf23@uea.edu.br>
2025-02-26 18:21:06 -0400

    add rubens qrcode-reader changes

--- frontend/src/components/layouts/DefaultLayout.tsx
+++ frontend/src/components/layouts/DefaultLayout.tsx
@@ -9,7 +9,7 @@ export function DefaultLayout({ children }: DefaultLayoutProps) {
   return (
     <div className="w-screen h-screen grid grid-cols-[20%_80%] space-x-8 bg-default p-20 child:rounded-lg">
       <aside className="grid grid-rows-[20%_80%] space-y-8 child:rounded-lg">
-        <UserCard avatar={defaultAvatar} name="Benicio" surName="Mozan" />
+        <UserCard avatar={defaultAvatar} name="Pedro" surName="Yutaro" />
         <Menu />
       </aside>
       <main className="rounded-lg bg-white">{children}</main>
