401f6dede53426da72c2a3730465f5dd9bd76bf5
Vinícius Castro Coutinho <vcc.snf23@uea.edu.br>
2025-03-02 01:50:20 -0400

    começando a página dos relatórios
    fingindo que eu sei o que to fazendo

--- frontend/src/components/pages/Reports.tsx
+++ frontend/src/components/pages/Reports.tsx
@@ -1,43 +1,50 @@
+import ReportIcon from "@/assets/Icon_simple_everplaces.svg"
+import ClockIcon from "@/assets/Icon material-access-time.svg"
 import { DefaultLayout } from "../layouts/DefaultLayout";
-
-// type Props = {
-//   tray: number;
-//   stations: number;
-//   cameras: number;
-// };
+import { Button } from "../ui/button";
+import { FileChartPie } from "lucide-react";
+import styles from "./Reports.module.css"
 
 export function Reports() {
-  // const navigate = useNavigate();
-  // const [registered, setRegistered] = useState<Props>({
-  //   tray: 0,
-  //   stations: 0,
-  //   cameras: 0,
-  // });
-  // const [periodo, setPeriodo] = useState("mensal");
-
-  // async function fetchQuantity() {
-  //   try {
-  //     const cameras = await api.get<[]>("/camera-managment");
-  //     const stations = await api.get<[]>("/station-managment");
-  //     const trays = await api.get<[]>("/tray-managment");
-
-  //     setRegistered({
-  //       tray: trays.data.length,
-  //       stations: stations.data.length,
-  //       cameras: cameras.data.length,
-  //     });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   fetchQuantity();
-  // }, [registered]);
 
   return (
     <DefaultLayout>
-      <div className="">content</div>
+        <div className={styles.tela}>
+
+          <div className={styles.ocorrencias}>
+
+            <div className={styles.ocorrencia}>
+              <header className={styles.cabeçalho}>
+                <img src={ReportIcon} alt="ReportIcon" className={styles.icone_grande}/>
+                <h1 className={styles.titulo}>Ocorrência por Setores</h1>
+                <Button className="bg-transparent border-gray-400 border-2 text-gray-dark rounded-2xl h-[18px] text-[10px] mt-auto mb-auto hover:bg-green-300 "> 
+                        <FileChartPie className=" text-gray-dark" size={15}/> Gerar Relatório
+                </Button>
+              </header>
+
+              <div className={styles.grafico}>
+              </div>
+            </div>
+
+            <div className={styles.ocorrencia}>
+              <header className={styles.cabeçalho}>
+                <img src={ClockIcon} alt="ClockIcon" className={styles.icone_grande}/>
+                <h1 className={styles.titulo}>Ocorrência por Tempo</h1>
+                <Button className="bg-transparent border-gray-400 border-2 text-gray-dark rounded-2xl h-[18px] text-[10px] mt-auto mb-auto hover:bg-green-300 "> 
+                        <FileChartPie className=" text-gray-dark" size={15}/> Gerar Relatório
+                </Button>
+              </header>
+
+              <div className={styles.grafico}>
+              </div>
+            </div>
+
+          </div>
+          
+          <div className={styles.historico}>
+            
+          </div>
+        </div>
     </DefaultLayout>
   );
 }
