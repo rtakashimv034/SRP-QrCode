6c53e1ef270aa985e80265ba9171fa23dc0c3165
rtakashimv034 <rtmv.snf23@uea.edu.br>
2025-03-06 20:30:05 -0400

    fix-qrcode-reader

--- lerQrCode/main.py
+++ lerQrCode/main.py
@@ -13,6 +13,8 @@ import json
 # Variável global para armazenar os QR codes lidos
 qrcodes = []  # Lista única para todas as câmeras
 product_path = []  # Lista para armazenar os caminhos dos produtos
+defective_product_path = [] # Lista para armazenar os produtos defeituosos
+PUBLIC = 'srpapp.duckdns.org'
 lock = threading.Lock()  # Para evitar problemas de concorrência
 
 # Sinalizador para controlar a execução das threads
@@ -25,7 +27,7 @@ CORS(app)
 caps = {}
 
 def read_qr_code(camera_index):
-    global qrcodes, running, product_path
+    global qrcodes, running, product_path, defective_product_path
     cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
     
     if not cap.isOpened():
@@ -48,6 +50,7 @@ def read_qr_code(camera_index):
         decoded_objects = pyzbar.decode(frame)
         found_bdj = None
         found_produto = None
+        found_defective_produto = None
 
         for obj in decoded_objects:
             data = obj.data.decode('utf-8')
@@ -69,6 +72,8 @@ def read_qr_code(camera_index):
                 found_bdj = data
             elif data.startswith('P'):  # Supondo que Produto começa com 'P'
                 found_produto = data
+            elif data.startswith('D'): #Supondo que o produto defeituoso começa com 'D':
+                found_defective_produto = data
 
             with lock:
                 camera_entry = next((entry for entry in qrcodes if entry["camera"] == camera_index), None)
@@ -88,52 +93,129 @@ def read_qr_code(camera_index):
                         camera_entry["BDJ"].append(bdj_entry)
                         print(f"Câmera {camera_index}: BDJ detectado - {data} ({timestamp})")
 
-        # Verifica se BDJ e Produto foram lidos no mesmo frame
         if found_bdj and found_produto:
-            #print(f"[!] Câmera {camera_index}: BDJ ({found_bdj}) e Produto ({found_produto}) DETECTADOS SIMULTANEAMENTE!")
-
             with lock:
-                # Encontra a estação (ET) associada ao BDJ
-                estacao = None
-                bdj_timestamp = None
+                # Lista para armazenar todos os caminhos válidos encontrados
+                novos_caminhos = []
+
+                # Percorre todas as entradas em qrcodes
                 for entry in qrcodes:
                     for bdj in entry["BDJ"]:
                         if bdj["data"] == found_bdj:
                             estacao = entry["ET"]
-                            bdj_timestamp = bdj["timestamp"]  # Timestamp da bandeja
-                            break
+                            bdj_timestamp = bdj["timestamp"]
 
-                # Adiciona o par [produto, estacao, timestamp_adicao, timestamp_bandeja] ao product_path, se não existir
-                if estacao and bdj_timestamp:
-                    now = datetime.now()
-                    #timestamp_adicao = now.strftime("%Y-%m-%d %H:%M:%S")  # Timestamp de adição "envio para o banco"
-                    novo_caminho = [found_produto, estacao, bdj_timestamp]
+                            # Cria um novo caminho
+                            novo_caminho = [found_produto, estacao, bdj_timestamp]
 
-                    # Verifica se o caminho já existe (ignorando os timestamps)
-                    if not any(caminho[0] == found_produto and caminho[1] == estacao for caminho in product_path):
-                        product_path.append(novo_caminho)
-                        print(f"Produto {found_produto} passou pela estação {estacao}.")
-                        print(f" Timestamp da bandeja: {bdj_timestamp}")
-                        print("Caminho do produto atualizado:", product_path)
+                            # Verifica se o caminho já existe (ignorando o timestamp)
+                            caminho_existente = any(
+                                caminho[0] == found_produto and caminho[1] == estacao
+                                for caminho in product_path
+                            )
 
+                            # Se o caminho não existir, adiciona à lista de novos caminhos
+                            if not caminho_existente:
+                                novos_caminhos.append(novo_caminho)
+
+                # Adiciona todos os novos caminhos ao product_path
+                if novos_caminhos:
+                    product_path.extend(novos_caminhos)
+                    print(f"Produto {found_produto} passou pelas estações: {[caminho[1] for caminho in novos_caminhos]}")
+                    print(f"Timestamps das bandejas: {[caminho[2] for caminho in novos_caminhos]}")
+                    print("Caminho do produto atualizado:", product_path)
+
+                # Remove o BDJ detectado de todas as câmeras
+                for entry in qrcodes:
+                    entry["BDJ"] = [bdj for bdj in entry["BDJ"] if bdj["data"] != found_bdj]
+                
+            # Itera sobre cada caminho no product_path
+            for path in product_path:
+                # Verifica se o caminho tem pelo menos 3 elementos (produto, estação e timestamp)
+
+                if len(path) >= 3:
+                    # Cria o dicionário com os dados do caminho
+                    dicionario = {
+                        "stationIdStr": path[1],  # Estação
+                        "prodSN": path[0],        # Produto
+                        "registeredAt": path[2],  # Timestamp
+                    }
+
+                    try:
+                        # Envia a requisição POST para a API
+                        response = requests.post(
+                            f'http://localhost:3333/api/v1/camera/path',
+                            json=dicionario,  # Passa o dicionário diretamente 
+                            timeout=10
+                        )
+
+                        # Verifica a resposta da API
+                        if response.status_code == 201:
+                            print("Post criado com sucesso!")
+                            print("Resposta do servidor:", response.json())  # Exibe a resposta JSON
+                        else:
+                            print("Erro ao criar o post:", response.status_code)
+                            print("Detalhes:", response.text)  # Exibe a resposta de erro, se houver
+
+                    except requests.exceptions.RequestException as e:
+                        # Captura erros específicos de requisição HTTP
+                        print("Ocorreu um erro ao enviar os dados para o banco:", str(e))
+                    except Exception as e:
+                        # Captura outros erros inesperados
+                        print("Ocorreu um erro inesperado:", str(e))
+
+            qq = []
+            found_produto = None
+        
+        if found_bdj and found_defective_produto:
+            with lock:
+                # Lista para armazenar todos os caminhos válidos encontrados
+                novos_caminhos = []
+
+                # Percorre todas as entradas em qrcodes
+                for entry in qrcodes:
+                    for bdj in entry["BDJ"]:
+                        if bdj["data"] == found_bdj:
+                            estacao = entry["ET"]
+                            bdj_timestamp = bdj["timestamp"]
+
+                            # Cria um novo caminho
+                            novo_caminho = [found_defective_produto, estacao, bdj_timestamp]
+
+                            # Verifica se o caminho já existe (ignorando o timestamp)
+                            caminho_existente = any(
+                                caminho[0] == found_defective_produto and caminho[1] == estacao
+                                for caminho in defective_product_path
+                            )
+
+                            # Se o caminho não existir, adiciona à lista de novos caminhos
+                            if not caminho_existente:
+                                novos_caminhos.append(novo_caminho)
+
+                # Adiciona todos os novos caminhos ao product_path
+                if novos_caminhos:
+                    defective_product_path.extend(novos_caminhos)
+                    print(f"Produto {found_defective_produto} passou pelas estações: {[caminho[1] for caminho in novos_caminhos]}")
+                    print(f"Timestamps das bandejas: {[caminho[2] for caminho in novos_caminhos]}")
+                    print("Caminho do produto atualizado:", defective_product_path)
 
                 # Remove o BDJ detectado de todas as câmeras
                 for entry in qrcodes:
                     entry["BDJ"] = [bdj for bdj in entry["BDJ"] if bdj["data"] != found_bdj]
 
-            for path in product_path:
+            for path in defective_product_path:
 
                             if len(path) >= 2:
                                 dicionario = {
-                                    "stationId" : path[1] ,
-                                    "prodSN": path[0],
+                                    "stationIdStr" : path[1] ,
+                                    "defProdIdStr": path[0],
                                     "registeredAt": path[2],
                                 }
                             
                             data = json.dumps(dicionario, indent=4)  # Converte o dicionário em JSON
                             print(data)
                             try: 
-                                response = requests.post('http://localhost:3333/api/v1/path', json=data)
+                                response = requests.post(f'http://localhost:3333/api/v1/camera/defective-path', json=data) # caminho defeituoso 
 
                                 # Verificando a resposta
                                 if response.status_code == 201:
@@ -145,7 +227,8 @@ def read_qr_code(camera_index):
                             except:
                                 print("ocorreu um erro para passar os dados para o banco")
 
-        
+            defective_product_path = []
+            found_defective_produto = None
 
         # Exibe a imagem da câmera com os retângulos
         cv2.imshow(window_name, frame)
@@ -172,34 +255,34 @@ for index in camera_indices:
     thread.start()
     threads.append(thread)
 
-# Rota para retornar os QR codes lidos
-@app.route('/api/camera_path')
-def get_qr_codes():
-    global qrcodes, product_path
+# # Rota para retornar os QR codes lidos
+# @app.route('/api/camera_path')
+# def get_qr_codes():
+#     global qrcodes, product_path
 
-    print("Acessando /api/camera_path...")  # Debug
-    print("Conteúdo atual de qrcodes:", qrcodes)  # Debug
-    print("Caminho dos produtos:", product_path)  # Debug
+#     print("Acessando /api/camera_path...")  # Debug
+#     print("Conteúdo atual de qrcodes:", qrcodes)  # Debug
+#     print("Caminho dos produtos:", product_path)  # Debug
 
-    result = {
-        "qrcodes": [],
-        "product_path": product_path
-    }
+#     result = {
+#         "qrcodes": [],
+#         "product_path": product_path
+#     }
 
-    for entry in qrcodes:
-        if entry.get("ET") is not None:  # Garante que ET existe e não é None
-            result["qrcodes"].append({
-                "camera": entry["camera"],
-                "ET": entry["ET"],
-                "BDJ": entry.get("BDJ", [])  # Evita erro se BDJ não existir
-            })
+#     for entry in qrcodes:
+#         if entry.get("ET") is not None:  # Garante que ET existe e não é None
+#             result["qrcodes"].append({
+#                 "camera": entry["camera"],
+#                 "ET": entry["ET"],
+#                 "BDJ": entry.get("BDJ", [])  # Evita erro se BDJ não existir
+#             })
 
-    if result["qrcodes"] or result["product_path"]:
-        print("Retornando dados encontrados:", result)  # Debug
-        return jsonify(result)
-    else:
-        print("Nenhum dado encontrado.")  # Debug
-        return jsonify({"error": "Nenhum dado encontrado"})
+#     if result["qrcodes"] or result["product_path"]:
+#         print("Retornando dados encontrados:", result)  # Debug
+#         return jsonify(result)
+#     else:
+#         print("Nenhum dado encontrado.")  # Debug
+#         return jsonify({"error": "Nenhum dado encontrado"})
 
 # Função para encerrar o servidor corretamente
 def shutdown(signum, frame):
@@ -227,5 +310,4 @@ signal.signal(signal.SIGINT, shutdown)
 
 # Inicia o servidor Flask
 if __name__ == '__main__':
-    print("Servidor Flask iniciado.")
     app.run(debug=False)
