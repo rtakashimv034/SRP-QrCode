86fd148bc8019060df46f76232317b4bff1f0257
Matheus Takashi Maruoka Vieira <mtmv.snf23@uea.edu.br>
2025-03-21 14:22:30 -0400

    Versão final
    Versão mais atualizada

--- lerQrCode/main.py
+++ lerQrCode/main.py
@@ -1,35 +1,37 @@
-from flask import Flask, request, jsonify
 import cv2
 from pyzbar import pyzbar
 import threading
-from flask_cors import CORS
 import signal
 import sys
 from datetime import datetime
 import requests
 import json
+import pygame
 
+# Inicializar o mixer do pygame
+pygame.mixer.init()
+
+# Carregar o arquivo de áudio
+pygame.mixer.music.load("beep-08b.wav")
 
 # Variável global para armazenar os QR codes lidos
 qrcodes = []  # Lista única para todas as câmeras
 product_path = []  # Lista para armazenar os caminhos dos produtos
-defective_product_path = [] # Lista para armazenar os produtos defeituosos
+defective_product_path = []  # Lista para armazenar os produtos defeituosos
 PUBLIC = 'srpapp.duckdns.org'
 lock = threading.Lock()  # Para evitar problemas de concorrência
 
 # Sinalizador para controlar a execução das threads
 running = True
 
-app = Flask(__name__)
-CORS(app)
-
 # Variável para armazenar as capturas de vídeo
 caps = {}
+
 
 def read_qr_code(camera_index):
     global qrcodes, running, product_path, defective_product_path
     cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
-    
+
     if not cap.isOpened():
         print(f"Erro ao abrir a câmera {camera_index}.")
         return
@@ -57,14 +59,14 @@ def read_qr_code(camera_index):
             rect = obj.rect  # Pega as coordenadas do QR Code
 
             # Desenha um retângulo ao redor do QR Code
-            cv2.rectangle(frame, 
-                          (rect.left, rect.top), 
-                          (rect.left + rect.width, rect.top + rect.height), 
+            cv2.rectangle(frame,
+                          (rect.left, rect.top),
+                          (rect.left + rect.width, rect.top + rect.height),
                           (0, 255, 0), 2)  # Cor verde, espessura 2
 
             # Escreve o conteúdo do QR Code acima do retângulo
-            cv2.putText(frame, data, 
-                        (rect.left, rect.top - 10), 
+            cv2.putText(frame, data,
+                        (rect.left, rect.top - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
             # Identificar BDJ e Produto
@@ -72,13 +74,16 @@ def read_qr_code(camera_index):
                 found_bdj = data
             elif data.startswith('P'):  # Supondo que Produto começa com 'P'
                 found_produto = data
-            elif data.startswith('D'): #Supondo que o produto defeituoso começa com 'D':
+            # Supondo que o produto defeituoso começa com 'D':
+            elif data.startswith('D'):
                 found_defective_produto = data
 
             with lock:
-                camera_entry = next((entry for entry in qrcodes if entry["camera"] == camera_index), None)
+                camera_entry = next(
+                    (entry for entry in qrcodes if entry["camera"] == camera_index), None)
                 if not camera_entry:
-                    camera_entry = {"camera": camera_index, "ET": None, "BDJ": []}
+                    camera_entry = {"camera": camera_index,
+                                    "ET": None, "BDJ": []}
                     qrcodes.append(camera_entry)
 
                 if data.startswith('E') and camera_entry["ET"] is None:
@@ -91,7 +96,10 @@ def read_qr_code(camera_index):
                     bdj_entry = {"data": data, "timestamp": timestamp}
                     if not any(bdj["data"] == data for bdj in camera_entry["BDJ"]):
                         camera_entry["BDJ"].append(bdj_entry)
-                        print(f"Câmera {camera_index}: BDJ detectado - {data} ({timestamp})")
+                        print(
+                            f"Câmera {camera_index}: BDJ detectado - {data} ({timestamp})")
+                        # Reproduzir o som de forma não bloqueante
+                        pygame.mixer.music.play()
 
         if found_bdj and found_produto:
             with lock:
@@ -106,7 +114,8 @@ def read_qr_code(camera_index):
                             bdj_timestamp = bdj["timestamp"]
 
                             # Cria um novo caminho
-                            novo_caminho = [found_produto, estacao, bdj_timestamp]
+                            novo_caminho = [found_produto,
+                                            estacao, bdj_timestamp]
 
                             # Verifica se o caminho já existe (ignorando o timestamp)
                             caminho_existente = any(
@@ -121,14 +130,17 @@ def read_qr_code(camera_index):
                 # Adiciona todos os novos caminhos ao product_path
                 if novos_caminhos:
                     product_path.extend(novos_caminhos)
-                    print(f"Produto {found_produto} passou pelas estações: {[caminho[1] for caminho in novos_caminhos]}")
-                    print(f"Timestamps das bandejas: {[caminho[2] for caminho in novos_caminhos]}")
+                    print(
+                        f"Produto {found_produto} passou pelas estações: {[caminho[1] for caminho in novos_caminhos]}")
+                    print(
+                        f"Timestamps das bandejas: {[caminho[2] for caminho in novos_caminhos]}")
                     print("Caminho do produto atualizado:", product_path)
 
                 # Remove o BDJ detectado de todas as câmeras
                 for entry in qrcodes:
-                    entry["BDJ"] = [bdj for bdj in entry["BDJ"] if bdj["data"] != found_bdj]
-                
+                    entry["BDJ"] = [bdj for bdj in entry["BDJ"]
+                                    if bdj["data"] != found_bdj]
+
             # Itera sobre cada caminho no product_path
             for path in product_path:
                 # Verifica se o caminho tem pelo menos 3 elementos (produto, estação e timestamp)
@@ -144,29 +156,34 @@ def read_qr_code(camera_index):
                     try:
                         # Envia a requisição POST para a API
                         response = requests.post(
-                            f'http://localhost:3333/api/v1/camera/path',
-                            json=dicionario,  # Passa o dicionário diretamente 
+                            f'http://20.10.70.151:3333/api/v1/camera/path',
+                            json=dicionario,  # Passa o dicionário diretamente
                             timeout=10
                         )
 
                         # Verifica a resposta da API
                         if response.status_code == 201:
                             print("Post criado com sucesso!")
-                            print("Resposta do servidor:", response.json())  # Exibe a resposta JSON
+                            pygame.mixer.music.play()
+                            # Exibe a resposta JSON
+                            print("Resposta do servidor:", response.json())
                         else:
-                            print("Erro ao criar o post:", response.status_code)
-                            print("Detalhes:", response.text)  # Exibe a resposta de erro, se houver
+                            print("Erro ao criar o post:",
+                                  response.status_code)
+                            # Exibe a resposta de erro, se houver
+                            print("Detalhes:", response.text)
 
                     except requests.exceptions.RequestException as e:
                         # Captura erros específicos de requisição HTTP
-                        print("Ocorreu um erro ao enviar os dados para o banco:", str(e))
+                        print(
+                            "Ocorreu um erro ao enviar os dados para o banco:", str(e))
                     except Exception as e:
                         # Captura outros erros inesperados
                         print("Ocorreu um erro inesperado:", str(e))
 
             qq = []
             found_produto = None
-        
+
         if found_bdj and found_defective_produto:
             with lock:
                 # Lista para armazenar todos os caminhos válidos encontrados
@@ -180,7 +197,8 @@ def read_qr_code(camera_index):
                             bdj_timestamp = bdj["timestamp"]
 
                             # Cria um novo caminho
-                            novo_caminho = [found_defective_produto, estacao, bdj_timestamp]
+                            novo_caminho = [
+                                found_defective_produto, estacao, bdj_timestamp]
 
                             # Verifica se o caminho já existe (ignorando o timestamp)
                             caminho_existente = any(
@@ -195,37 +213,46 @@ def read_qr_code(camera_index):
                 # Adiciona todos os novos caminhos ao product_path
                 if novos_caminhos:
                     defective_product_path.extend(novos_caminhos)
-                    print(f"Produto {found_defective_produto} passou pelas estações: {[caminho[1] for caminho in novos_caminhos]}")
-                    print(f"Timestamps das bandejas: {[caminho[2] for caminho in novos_caminhos]}")
-                    print("Caminho do produto atualizado:", defective_product_path)
+                    print(
+                        f"Produto {found_defective_produto} passou pelas estações: {[caminho[1] for caminho in novos_caminhos]}")
+                    print(
+                        f"Timestamps das bandejas: {[caminho[2] for caminho in novos_caminhos]}")
+                    print("Caminho do produto atualizado:",
+                          defective_product_path)
 
                 # Remove o BDJ detectado de todas as câmeras
                 for entry in qrcodes:
-                    entry["BDJ"] = [bdj for bdj in entry["BDJ"] if bdj["data"] != found_bdj]
+                    entry["BDJ"] = [bdj for bdj in entry["BDJ"]
+                                    if bdj["data"] != found_bdj]
 
             for path in defective_product_path:
 
-                            if len(path) >= 2:
-                                dicionario = {
-                                    "stationIdStr" : path[1] ,
-                                    "defProdIdStr": path[0],
-                                    "registeredAt": path[2],
-                                }
-                            
-                            data = json.dumps(dicionario, indent=4)  # Converte o dicionário em JSON
-                            print(data)
-                            try: 
-                                response = requests.post(f'http://localhost:3333/api/v1/camera/defective-path', json=data) # caminho defeituoso 
+                if len(path) >= 2:
+                    dicionario = {
+                        "stationIdStr": path[1],
+                        "defProdIdStr": path[0],
+                        "registeredAt": path[2],
+                    }
 
-                                # Verificando a resposta
-                                if response.status_code == 201:
-                                    print("Post criado com sucesso!")
-                                    print("Resposta do servidor:", response.json())  # Exibe a resposta JSON
-                                else:
-                                    print("Erro ao criar o post:", response.status_code)
-                                    print("Detalhes:", response.text)  # Exibe a resposta de erro, se houver
-                            except:
-                                print("ocorreu um erro para passar os dados para o banco")
+                # Converte o dicionário em JSON
+                data = json.dumps(dicionario, indent=4)
+                print(data)
+                try:
+                    response = requests.post(
+                        f'http://20.10.70.151:3333/api/v1/camera/defective-path', json=data)  # caminho defeituoso
+
+                    # Verificando a resposta
+                    if response.status_code == 201:
+                        print("Post criado com sucesso!")
+                        pygame.mixer.music.play()
+                        # Exibe a resposta JSON
+                        print("Resposta do servidor:", response.json())
+                    else:
+                        print("Erro ao criar o post:", response.status_code)
+                        # Exibe a resposta de erro, se houver
+                        print("Detalhes:", response.text)
+                except:
+                    print("ocorreu um erro para passar os dados para o banco")
 
             defective_product_path = []
             found_defective_produto = None
@@ -238,6 +265,7 @@ def read_qr_code(camera_index):
 
     cap.release()
     cv2.destroyWindow(window_name)
+
 
 # Verifica quais câmeras estão disponíveis
 camera_indices = []
@@ -255,34 +283,6 @@ for index in camera_indices:
     thread.start()
     threads.append(thread)
 
-# # Rota para retornar os QR codes lidos
-# @app.route('/api/camera_path')
-# def get_qr_codes():
-#     global qrcodes, product_path
-
-#     print("Acessando /api/camera_path...")  # Debug
-#     print("Conteúdo atual de qrcodes:", qrcodes)  # Debug
-#     print("Caminho dos produtos:", product_path)  # Debug
-
-#     result = {
-#         "qrcodes": [],
-#         "product_path": product_path
-#     }
-
-#     for entry in qrcodes:
-#         if entry.get("ET") is not None:  # Garante que ET existe e não é None
-#             result["qrcodes"].append({
-#                 "camera": entry["camera"],
-#                 "ET": entry["ET"],
-#                 "BDJ": entry.get("BDJ", [])  # Evita erro se BDJ não existir
-#             })
-
-#     if result["qrcodes"] or result["product_path"]:
-#         print("Retornando dados encontrados:", result)  # Debug
-#         return jsonify(result)
-#     else:
-#         print("Nenhum dado encontrado.")  # Debug
-#         return jsonify({"error": "Nenhum dado encontrado"})
 
 # Função para encerrar o servidor corretamente
 def shutdown(signum, frame):
@@ -305,9 +305,10 @@ def shutdown(signum, frame):
     # Encerra o programa
     sys.exit(0)
 
+
 # Captura o sinal de interrupção (Ctrl+C)
 signal.signal(signal.SIGINT, shutdown)
 
-# Inicia o servidor Flask
-if __name__ == '__main__':
-    app.run(debug=False)
+# Mantém o programa em execução
+while running:
+    pass
